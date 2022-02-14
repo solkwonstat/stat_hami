1a33ab17de878f98c99d6732538e19d79559f5eb
Sol Kwon <sol.kwon@gausslabs.ai>
2022-02-14 15:44:18 +0900

    PR test commit
    - rebasing and squashing previous PR test commits

--- experimental/test.py
+++ experimental/test.py
@@ -1,1 +1,7 @@
-print("hello world")
+import logging
+import time
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    logging.info("hello world")
+    logging.info("Current time is %s", int(time.time()))
